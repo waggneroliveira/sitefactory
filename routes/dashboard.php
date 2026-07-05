a4f830f48f23e023bab6d851cecac7f197425e81
Front-end-4 <waggner.dev@gmail.com>
2026-07-05 15:42:57 -0300

    Update dashboard.php

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -45,12 +45,6 @@ use App\Http\Controllers\LetsgoController;
 use App\Http\Controllers\ServiceLocationController;
 use App\Http\Controllers\SessaoFaqController;
 
-// Route::domain('localhost')->group(function () {
-//     Route::get('/', function (string $tenant) {
-//         return "Localhost";
-//     });
-// });
-
 Route::domain('{tenant}.localhost')->group(function () {
     Route::get('/painel', function (string $tenant) {
         return redirect()->route('admin.dashboard.painel');
